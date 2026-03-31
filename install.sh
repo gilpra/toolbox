081ccc5d4902c5a0eb1102be61f00dea75e4c287
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-01 03:38:46 +0700

    feat: configure btrfs subvolumes and mount layout

--- install.sh
+++ install.sh
@@ -21,3 +21,27 @@ read -r -p "Input timezone (example: Asia/Jakarta): " TIMEZONE
 read -r -p "Enter new username: " USERNAME
 read -r -s -p "Set root password: " ROOT_PASS;  echo
 read -r -s -p "Set password for $USERNAME: " USER_PASS; echo
+
+# === FORMAT & MOUNT ===
+mkfs.fat -F32 "$PATH_BOOT"
+mkfs.btrfs -f "$PATH_LINUX"
+
+mount "$PATH_LINUX" /mnt
+
+# Buat subvolume (tanpa snapshots)
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@cache
+btrfs subvolume create /mnt/@tmp
+umount /mnt
+
+# Mount subvolumes
+mount -o noatime,compress=zstd:3,subvol=@       "$PATH_LINUX" /mnt
+mkdir -p /mnt/{boot/efi,home,var/log,var/cache,tmp,var/lib}
+
+mount -o noatime,compress=zstd:3,autodefrag,subvol=@home   "$PATH_LINUX" /mnt/home
+mount -o noatime,compress=zstd:3,subvol=@log               "$PATH_LINUX" /mnt/var/log
+mount -o noatime,compress=zstd:3,subvol=@cache             "$PATH_LINUX" /mnt/var/cache
+mount -o noatime,compress=zstd:3,subvol=@tmp               "$PATH_LINUX" /mnt/tmp
+mount "$PATH_BOOT" /mnt/boot/efi

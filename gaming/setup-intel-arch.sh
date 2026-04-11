9d15e8ef9a572ac2bca4a44831c639f6b2117647
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-11 20:43:11 +0700

    feat: configure zram-generator

--- gaming/setup-intel-arch.sh
+++ gaming/setup-intel-arch.sh
@@ -22,3 +22,28 @@ echo
 echo "Optimization GRUB..."
 sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="nowatchdog zswap.enabled=0 loglevel=3 quiet"/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+echo
+echo "Installing zram..."
+sudo pacman -S --noconfirm --needed zram-generator
+
+echo
+echo "Setup zram-generator..."
+[ -f /etc/systemd/zram-generator.conf ] && sudo cp /etc/systemd/zram-generator.conf /etc/systemd/zram-generator.conf.bak
+sudo tee /etc/systemd/zram-generator.conf >/dev/null <<EOF
+[zram0]
+zram-size = ram
+compression-algorithm = zstd
+swap-priority = 100
+fs-type = swap
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl start systemd-zram-setup@zram0.service
+
+echo
+echo "Checking zram..."
+swapon --summary
+
+echo
+echo "Setup gaming for intel archlinux complete!"

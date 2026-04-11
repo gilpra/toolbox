45dd8ef397c0ed49f8f6be72494196a94f7f2734
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-11 20:21:43 +0700

    feat: optimize GRUB kernel parameters

--- gaming/setup-intel-arch.sh
+++ gaming/setup-intel-arch.sh
@@ -17,3 +17,8 @@ sudo pacman -S --noconfirm --needed mesa vulkan-intel intel-media-driver intel-u
 echo
 echo "Installing lib32 package..."
 sudo pacman -S --noconfirm --needed lib32-mesa lib32-vulkan-intel lib32-vulkan-mesa-layers lib32-freetype2
+
+echo
+echo "Optimization GRUB..."
+sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="nowatchdog zswap.enabled=0 loglevel=3 quiet"/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg

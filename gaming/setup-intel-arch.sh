707e22128051318c3f9b2e17c66e27bdc942c4b1
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-11 20:05:11 +0700

    feat: install 32-bit gaming library

--- gaming/setup-intel-arch.sh
+++ gaming/setup-intel-arch.sh
@@ -13,3 +13,7 @@ sudo pacman -Syu --noconfirm
 echo
 echo "Installing intel package..."
 sudo pacman -S --noconfirm --needed mesa vulkan-intel intel-media-driver intel-ucode
+
+echo
+echo "Installing lib32 package..."
+sudo pacman -S --noconfirm --needed lib32-mesa lib32-vulkan-intel lib32-vulkan-mesa-layers lib32-freetype2

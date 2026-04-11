454c354259313db3c5eeb3e63606139a60002817
Gilang Pradana <gilangarya272001@gmail.com>
2026-04-11 19:43:23 +0700

    feat: enable multilib repo

---
+++ gaming/setup-intel-arch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! grep -q "^\[multilib\]$" /etc/pacman.conf; then
+  echo
+  echo "Enable multilib..."
+  sudo cp /etc/pacman.conf /etc/pacman.conf.bak
+  sudo sed -i '/^#\[multilib\]/,/^#Include/s/^#//' /etc/pacman.conf
+fi
+sudo pacman -Syu --noconfirm

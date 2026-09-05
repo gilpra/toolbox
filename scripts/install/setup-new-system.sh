0f67126fefa964e24a983e1d19a073c25e0dfdee
Gilang Pradana <gilangarya272001@gmail.com>
2026-09-06 01:49:08 +0700

    perf(new-system): use shallow clones for faster setup

--- scripts/install/setup-new-system.sh
+++ scripts/install/setup-new-system.sh
@@ -10,11 +10,11 @@ ssh-keygen -t ed25519
 echo "Setup neovim with dotfiles..."
 sudo pacman -S --needed neovim tree-sitter-cli nodejs npm
 rm -rf ~/.config/nvim
-git clone https://github.com/gilpra/nvim.git ~/.config/nvim
+git clone --depth 1 https://github.com/gilpra/nvim.git ~/.config/nvim
 
 echo "Install sway dotfiles..."
 rm -rf ~/.dotfiles/sway-dots
-git clone https://github.com/gilpra/sway-dots ~/.dotfiles/sway-dots
+git clone --depth 1 https://github.com/gilpra/sway-dots ~/.dotfiles/sway-dots
 bash ~/.dotfiles/sway-dots/setup.sh
 
 echo "Setup folder..."
@@ -24,7 +24,7 @@ mkdir -p ~/Projects/Programming/ ~/Games/
 echo "Setup tmux..."
 sudo pacman -S tmux
 curl https://gist.githubusercontent.com/gilpra/148276c20141b185097d34b268d93349/raw/f7d3f5d5c3b80d876c024af00dd3bbeb74412263/.tmux.conf -o ~/.tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Installing firefox..."
 sudo pacman -S --needed firefox

24349faf455bb9c9ceb17af6bb60a53f5574b9b9
Gilang Pradana <gilangarya272001@gmail.com>
2026-09-18 18:12:33 +0700

    refactor(setup-new): simplify script and consolidate package

--- system/setup-new-system.sh
+++ system/setup-new-system.sh
@@ -2,29 +2,45 @@
 set -euo pipefail
 
 echo "Installing package..."
-sudo pacman -S --needed ripgrep fzf less jq inxi openssh noto-fonts noto-fonts-emoji duf lazygit
+sudo pacman -S --needed \
+    ripgrep \
+    fzf \
+    less \
+    jq \
+    inxi \
+    openssh \
+    noto-fonts \
+    noto-fonts-emoji \
+    duf \
+    lazygit \
+    neovim \
+    tree-sitter-cli \
+    nodejs \
+    npm \
+    tmux
 
-echo "Setup ssh for Github..."
+echo "Generate ssh key..."
 ssh-keygen -t ed25519
 
 echo "Setup neovim with dotfiles..."
-sudo pacman -S --needed neovim tree-sitter-cli nodejs npm
 rm -rf ~/.config/nvim
 git clone --depth 1 https://github.com/gilpra/nvim.git ~/.config/nvim
 
-echo "Install sway dotfiles..."
-rm -rf ~/.dotfiles/sway-dots
-git clone --depth 1 https://github.com/gilpra/sway-dots ~/.dotfiles/sway-dots
-bash ~/.dotfiles/sway-dots/setup.sh
+echo "Install dotfiles..."
+rm -rf ~/.dotfiles
+git clone --depth 1 https://github.com/gilpra/dotfiles.git ~/.dotfiles
 
 echo "Setup folder..."
 xdg-user-dirs-update
-mkdir -p ~/Projects/Programming/ ~/Games/
+mkdir -p ~/Games/
 
 echo "Setup tmux..."
-sudo pacman -S tmux
 curl https://gist.githubusercontent.com/gilpra/148276c20141b185097d34b268d93349/raw/f7d3f5d5c3b80d876c024af00dd3bbeb74412263/.tmux.conf -o ~/.tmux.conf
+rm -rf ~/.tmux/plugins/tpm
 git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "Installing firefox..."
-sudo pacman -S --needed firefox
+echo ""
+echo "==========================================================="
+echo "Your dotfiles are located in ~/.dotfiles"
+echo "To set up your dotfiles, read the README in that directory"
+echo "==========================================================="
